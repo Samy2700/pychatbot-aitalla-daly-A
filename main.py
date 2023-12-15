0365c35faf29f600df03447dc98e1574dbec299d
samy2700 <samy.ait-alla@efrei.net>
2023-12-15 16:38:40 +0100

    Matrice question + génération de réponse

--- main.py
+++ main.py
@@ -68,14 +68,14 @@ for fichier in os.listdir(source_directory):
         destination_chemin_fichier = os.path.join(destination_directory, fichier)
 
         # Lire le contenu du fichier source
-        with open(source_chemin_fichier, 'r') as f:
+        with open(source_chemin_fichier, 'r', encoding='utf-8') as f:
             texte = f.read()
 
         # Convertir le texte
         texte_converti = texte_modifié(texte)
 
         # Écrire le texte converti dans le fichier de destination
-        with open(destination_chemin_fichier, 'w') as f:
+        with open(destination_chemin_fichier, 'w', encoding='utf-8') as f:
             f.write(texte_converti)
 
         # Afficher un message pour chaque fichier traité
@@ -103,7 +103,7 @@ for fichier in os.listdir(destination_directory):
     if fichier.endswith(".txt"):
         chemin_fichier = os.path.join(destination_directory, fichier)
         # Ouvrir et lire le contenu du fichier
-        with open(chemin_fichier, 'r') as f:
+        with open(chemin_fichier, 'r', encoding='utf-8') as f:
             texte = f.read()
 
         # Calculer la fréquence de chaque mot dans le texte
@@ -151,56 +151,44 @@ print("\n")
 
 def matrice_tf_idf(destination_directory):
     # Fonction pour créer une matrice TF-IDF
-    tf_idf_matrice = {}
+    tf_idf_matrice_corpus = {}
     # Récupérer la liste des fichiers texte dans le dossier de destination
     fichiers = [filename for filename in os.listdir(destination_directory) if filename.endswith(".txt")]
 
     # Calculer les scores TF pour chaque mot dans chaque fichier
     for fichier in fichiers:
         chemin_fichier = os.path.join(destination_directory, fichier)
-        with open(chemin_fichier, 'r') as f:
+        with open(chemin_fichier, 'r', encoding='utf-8') as f:
             tf_scores = score_tf(f.read())
             for mot in tf_scores:
                 # Initialiser la matrice pour chaque mot
-                if mot not in tf_idf_matrice:
-                    tf_idf_matrice[mot] = [0] * len(fichiers)
+                if mot not in tf_idf_matrice_corpus:
+                    tf_idf_matrice_corpus[mot] = [0] * len(fichiers)
                 # Calculer le score TF-IDF pour chaque mot dans chaque fichier
                 indice_fichier = fichiers.index(fichier)
-                tf_idf_matrice[mot][indice_fichier] = tf_scores[mot] * idf_resultats.get(mot,0)
+                tf_idf_matrice_corpus[mot][indice_fichier] = tf_scores[mot] * idf_resultats.get(mot,0)
 
-    return tf_idf_matrice, fichiers
+    return tf_idf_matrice_corpus, fichiers
 
 
-def transposer_matrice(tf_idf_matrice, fichiers):
-    # Fonction pour transposer la matrice TF-IDF
-    transposee = []
-    for i in range(len(fichiers)):
-        # Créer une ligne pour chaque fichier
-        ligne = [tf_idf_matrice[mot][i] for mot in tf_idf_matrice]
-        transposee.append(ligne)
+def affichage_matrice_tf_idf_corpus(tf_idf_matrice_corpus, fichiers):
+    largeur_mot = 35
 
-    return transposee
+    # Créer l'en-tête avec les mots (colonnes)
+    en_tete = f"{'Fichier':<{largeur_mot}}" + "".join(f"{mot:{largeur_mot}}" for mot in tf_idf_matrice_corpus.keys())
+    print(en_tete)
 
-# Appel de la fonction pour créer la matrice TF-IDF
+    # Afficher les scores TF-IDF pour chaque fichier
+    for index_fichier in range(len(fichiers)):
+        fichier = fichiers[index_fichier]
+        ligne = f"{fichier:<{largeur_mot}}" + "".join(
+            f"{tf_idf_matrice_corpus[mot][index_fichier]:<{largeur_mot}.2f}" for mot in tf_idf_matrice_corpus.keys())
+        print(ligne)
+    print("\n")
+
+# Appel de la fonction pour créer la matrice TF-IDF et afficher la matrice
 tf_idf, fichiers = matrice_tf_idf(destination_directory)
-matrice_transposee = transposer_matrice(tf_idf, fichiers)
-
-# Affichage de la matrice TF-IDF transposée
-largeur_mot = 35
-
-# Créer l'en-tête avec les mots
-en_tetes = "|".join(f"{mot:{largeur_mot}}" for mot in tf_idf.keys())
-print(f"{'Fichier: /   Mot : ':{largeur_mot}}" + en_tetes)
-
-# Afficher les scores TF-IDF pour chaque fichier
-index_fichier = 0
-while index_fichier < len(fichiers):
-    fichier = fichiers[index_fichier]
-    ligne = f"{fichier:<{largeur_mot}}" + "|".join(f"{score:<{largeur_mot}.2f}" for score in matrice_transposee[index_fichier])
-    print(ligne)
-    index_fichier += 1
-
-print("\n")
+affichage_matrice_tf_idf_corpus(tf_idf, fichiers)
 
 
 def mots_moins_importants(tf_idf):
@@ -249,7 +237,7 @@ def mot_chirac(destination_directory,textes_chirac):
     # Parcourir les textes associés à Chirac
     for fichier in textes_chirac:
         chemin_fichier = os.path.join(destination_directory, fichier)
-        with open(chemin_fichier, 'r') as f:
+        with open(chemin_fichier, 'r', encoding='utf-8') as f:
             texte = f.read()
             # Calculer la fréquence de chaque mot
             comptage = score_tf(texte)
@@ -281,7 +269,7 @@ def occurences_nation(destination_directory, mot_a_chercher):
     for fichier in os.listdir(destination_directory):
         if fichier.endswith(".txt"):
             chemin_fichier = os.path.join(destination_directory, fichier)
-            with open(chemin_fichier, 'r') as f:
+            with open(chemin_fichier, 'r', encoding='utf-8') as f:
                 contenu = f.read()
                 mots = contenu.split()
                 occurrences = mots.count(mot_a_chercher)
@@ -306,46 +294,36 @@ def premier_president_a_parler(mots_cles):
     for fichier in os.listdir(destination_directory):
         if fichier.endswith(".txt"):
             chemin_fichier = os.path.join(destination_directory, fichier)
-            with open(chemin_fichier, 'r') as f:
+            with open(chemin_fichier, 'r', encoding='utf-8') as f:
                 contenu = f.read()
                 # Vérifier si l'un des mots clés est présent dans le contenu du fichier
                 for mot_cle in mots_cles:
                     if mot_cle in contenu:
                         # Extraire le nom du président à partir du nom du fichier
-                        nom_fichier = os.path.splitext(fichier)[0] # Séparer le nom du fichier de son extension
-                        return nom_fichier.split("_")[1] # Retourner le nom du président
+                        nom_president = os.path.splitext(fichier)[0].split("_")[1] # Séparer le nom du fichier de son extension
+                        print(f"Le premier président à parler du climat et/ou de l'écologie est {nom_president}.")
+                        return # Retourner le nom du président
 
-
-def afficher_premier_president(mots_cles):
-    # Appeler la fonction premier_president_a_parler pour obtenir le nom du premier président parlant des mots clés
-    premier_president = premier_president_a_parler(mots_cles)
-
-    # Afficher le résultat
-    if premier_president:
-        print(f"Le premier président à parler du climat et/ou de l'écologie est {premier_president}.")
 
 
 def mots_communs_presidents(destination_directory, listes_textes_presidents, tf_idf):
     mots_non_importants = set(mot for mot, scores in tf_idf.items() if all(score == 0 for score in scores))
-    ensembles_mots_presidents = []
+    compteur_mots = {}
+    total_presidents = len(listes_textes_presidents)
 
-    # Créer des ensembles de mots pour chaque président
     for textes_president in listes_textes_presidents:
-        ensemble_mots = set()
+        mots_uniques = set()
         for fichier in textes_president:
             chemin_fichier = os.path.join(destination_directory, fichier)
-            with open(chemin_fichier, 'r') as f:
+            with open(chemin_fichier, 'r', encoding='utf-8') as f:
                 texte = f.read()
-                mots = set(score_tf(texte).keys())
-                ensemble_mots = ensemble_mots.union(mots)
+                for mot in score_tf(texte).keys():
+                    mots_uniques.add(mot)
 
-        ensembles_mots_presidents.append(ensemble_mots)
+        for mot in mots_uniques:
+            compteur_mots[mot] = compteur_mots.get(mot, 0) + 1
 
-    # Trouver les mots communs à tous les présidents
-    mots_communs = set.intersection(*ensembles_mots_presidents)
-
-    # Exclure les mots non importants
-    mots_communs_important = mots_communs - mots_non_importants
+    mots_communs_important = {mot for mot, compte in compteur_mots.items() if compte == total_presidents and mot not in mots_non_importants}
 
     print("Mots énoncés par tous les présidents (hors mots non importants) :", mots_communs_important)
 
@@ -360,7 +338,7 @@ def traiter_question(question_utilisateur):
             question += chr(ord(char) + ord("a") - ord("A"))
         # Ignorer les virgules, points, points-virgules et guillemets
         elif char in [",", ".", ";", '"', "?", "!"]:
-            question += ""
+            continue
         # Remplacer les tirets et les underscores par des espaces
         elif char in ["-", "_"]:
             question += " "
@@ -372,12 +350,13 @@ def traiter_question(question_utilisateur):
             question += char
 
     # Séparer la chaîne de caractères traitée en mots
-    question = question.split()
+    question_traitee = question.split()
 
-    return question
+    return question_traitee
 
 
 def intersection_question_corpus(tf_idf):
+    question_traitee = traiter_question(question_utilisateur)
     mot_question_dans_corpus = []
     for mot_questions in question_traitee:
         for mot, scores in tf_idf.items():
@@ -387,34 +366,213 @@ def intersection_question_corpus(tf_idf):
     print("Les mots communs sont :", mot_question_dans_corpus)
 
 
-def calculer_tf_question(question_traitee):
-    tf_question = {}
-
-    for mot in question_traitee:
-        tf_question[mot] = (tf_question.get(mot, 0)) / (len(question_utilisateur))
-
-    print(len(question_traitee))
-    return tf_question
-
-
-
-def tf_idf_question(tf_question, idf_resultats):
-    tf_idf_question = {}
-
-    for mot in tf_question:
-        if mot in idf_resultats:
-            tf_idf_question[mot] = tf_question[mot] * idf_resultats[mot]
+def score_tf_question(question_traitee):
+    # Fonction pour calculer la fréquence de chaque mot dans une question
+    compteur = {}
+    mots = question_traitee
+    for mot in mots:
+        # Si le mot est déjà dans le compteur, incrémenter sa valeur
+        if mot in compteur:
+            compteur[mot] += 1
+        # Sinon, initialiser sa valeur à 1
         else:
-            tf_idf_question[mot] = 0
+            compteur[mot] = 1
+    return compteur
 
-        # Ajouter les mots du corpus qui ne sont pas présents dans la question
-        for mot in idf_resultats:
-            if mot not in tf_question:
-                tf_idf_question[mot] = 0
+def score_idf_question(question_traitee, idf_resultats):
+    # Fonction pour calculer le score IDF de chaque mot dans une question
+    scores_idf = {}
+    mots = question_traitee
+    for mot in mots:
+        if mot in idf_resultats:
+            scores_idf[mot] = idf_resultats[mot]
+        else:
+            scores_idf[mot] = 0
+    return scores_idf
+
+def matrice_tf_idf_question(question_traitee, idf_resultats):
+    tf_idf_matrice_question = {}
+    fichiers = [filename for filename in os.listdir(destination_directory) if filename.endswith(".txt")]
+
+    # Construire une liste de tous les mots dans l'ordre de leur apparition dans le corpus
+    mots_corpus = []
+    for fichier in fichiers:
+        chemin_fichier = os.path.join(destination_directory, fichier)
+        with open(chemin_fichier, 'r', encoding='utf-8') as f:
+            mots = f.read().split()
+            for mot in mots:
+                if mot not in mots_corpus:
+                    mots_corpus.append(mot)
+                    tf_idf_matrice_question[mot] = 0
+
+    # Calculer les scores TF pour la question
+    tf_scores = score_tf(" ".join(question_traitee))
+
+    # Mettre à jour la matrice TF-IDF pour la question en utilisant les scores TF de la question
+    for mot in mots_corpus:
+        tf_score = tf_scores.get(mot, 0)
+        idf_score = idf_resultats.get(mot, 0)
+        tf_idf_matrice_question[mot] = tf_score * idf_score
+
+    return tf_idf_matrice_question
 
 
-    return tf_idf_question
+def afficher_matrice_tf_idf_question(tf_idf_question):
+    largeur_mot = 35
 
+    # Créer l'en-tête avec les mots (colonnes)
+    en_tete = f"{'Mot: ':<{largeur_mot}}" + "".join(f"{mot:{largeur_mot}}" for mot in tf_idf_question.keys())
+    print(en_tete)
+
+    # Afficher les scores TF-IDF pour la question
+    ligne = "".join(f"{tf_idf_question[mot]:<{largeur_mot}.2f}" for mot in tf_idf_question.keys())
+    print(f"{'Score TF-IDF de la question':<{largeur_mot}}" + ligne)
+
+
+def produit_scalaire(tf_idf_question, tf_idf, fichiers):
+    resultats = {}
+    for fichier in fichiers:
+        score = 0
+        indice_fichier = fichiers.index(fichier)  # Obtenir l'indice du fichier actuel
+        for mot in tf_idf_question:
+            score_tf_idf_question = tf_idf_question.get(mot, 0)
+            # Accès au score TF-IDF du mot pour le fichier spécifique
+            score_tf_idf_fichier = tf_idf.get(mot, [0] * len(fichiers))[indice_fichier]
+            score += score_tf_idf_question * score_tf_idf_fichier
+        resultats[fichier] = score
+    return resultats
+
+
+def calculer_norme_question(tf_idf_question):
+    if mot not in tf_idf:
+        return 0
+    # Initialiser la somme des carrés à 0
+    somme_carres = 0
+
+    # Additionner les carrés des scores TF-IDF
+    for score in tf_idf_question.values():
+        somme_carres += score ** 2
+
+    # Calculer la racine carrée de la somme des carrés pour obtenir la norme
+    norme = math.sqrt(somme_carres)
+
+    return norme
+
+
+def calculer_norme_corpus(tf_idf):
+    if mot not in tf_idf:
+        return 0  # Le mot n'est pas dans la matrice TF-IDF
+
+    scores_tf_idf = tf_idf[mot]
+    somme_carres = 0
+    for score in scores_tf_idf:
+        somme_carres = score ** 2
+
+    norme = math.sqrt(somme_carres)
+
+    return norme
+
+
+
+
+def calculer_similarite_cosinus(tf_idf_question, tf_idf, fichiers):
+    # Calcul du produit scalaire pour chaque fichier
+    resultats_produit_scalaire = produit_scalaire(tf_idf_question, tf_idf, fichiers)
+
+    # Calcul de la norme de la question
+    norme_question = calculer_norme_question(tf_idf_question)
+    norme_fichier = calculer_norme_corpus(tf_idf)  # Calcul de la norme du fichier
+    # Stockage des similarités cosinus
+    similarites = {}
+
+    # Calcul de la similarité cosinus pour chaque fichier
+    for fichier in fichiers:
+        if norme_fichier != 0 and norme_question != 0:
+            similarite = resultats_produit_scalaire[fichier] / (norme_fichier * norme_question)
+        else:
+            similarite = 0
+        similarites[fichier] = similarite
+
+    return similarites
+
+def trouver_document_le_plus_pertinent(tf_idf_question, tf_idf_corpus, fichiers):
+    similarites = calculer_similarite_cosinus(tf_idf_question, tf_idf_corpus, fichiers)
+
+    fichier_le_plus_pertinent = None
+    similarite_maximale = -1  # Initialiser à une valeur basse
+
+    for fichier, similarite in similarites.items():
+        if similarite > similarite_maximale:
+            similarite_maximale = similarite
+            fichier_le_plus_pertinent = fichier
+
+    return fichier_le_plus_pertinent
+
+
+
+def mot_avec_score_tfidf_le_plus_eleve(tf_idf_question):
+    mot_max = None
+    score_max = 0
+
+    for mot, score in tf_idf_question.items():
+        if score > score_max:
+            mot_max = mot
+            score_max = score
+
+    return mot_max
+
+
+def trouver_phrase_contenant_mot(source_directory, document, mot):
+    chemin_fichier = os.path.join(source_directory, document)
+    # Ouvrir et lire le fichier
+    with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
+        contenu = fichier.read()
+
+    # Trouver la première occurrence du mot
+    index_mot = contenu.find(mot)
+    if index_mot == -1:
+        return "Mot non trouvé dans le document."
+
+    # Trouver le début et la fin de la phrase
+    debut_phrase = contenu.rfind(".", 0, index_mot) + 1
+    fin_phrase = contenu.find(".", index_mot)
+
+    # Extraire et retourner la phrase
+    phrase = contenu[debut_phrase:fin_phrase + 1].strip()
+
+    return phrase
+
+
+
+
+
+import random
+
+def personnaliser_reponse(reponse_brute):
+    # Votre dictionnaire de formules de début de réponse
+    question_starters = {
+        "Comment": "Après analyse, ",
+        "Pourquoi": "Car, ",
+        "Peux-tu": "Oui, bien sûr! ",
+        # Vous pouvez ajouter d'autres formules ici
+    }
+
+    # Choisir une expression au hasard parmi les valeurs du dictionnaire
+    expression_choisie = random.choice(list(question_starters.values()))
+
+    # S'assurer que le premier caractère de la réponse brute est en minuscule
+    if reponse_brute:
+        reponse_brute = reponse_brute[0].lower() + reponse_brute[1:]
+
+    # Ajouter l'expression choisie au début de la réponse brute
+    reponse_personnalisee = expression_choisie + reponse_brute
+
+    # S'assurer que la réponse commence par une majuscule et se termine par un point
+    reponse_personnalisee = reponse_personnalisee[0] + reponse_personnalisee[1:]
+    if not reponse_personnalisee.endswith('.'):
+        reponse_personnalisee += '.'
+
+    return reponse_personnalisee
 
 
 
@@ -425,15 +583,45 @@ print("Question traitée :", question_traitee)
 
 intersection_question_corpus(tf_idf)
 
-tf_scores_question = calculer_tf_question(question_traitee)
-print("Les scores tf sont :", tf_scores_question)
+tf_score_question = score_tf_question(question_traitee)
+print("Les scores tf sont :", tf_score_question)
 
+idf_score_question = score_idf_question(question_traitee,idf_resultats)
+print("Les scores idf sont :", idf_score_question)
 
-matrice_tf_idf_question = tf_idf_question(tf_scores_question, idf_resultats)
-print("Les scores tf-idf sont : ", matrice_tf_idf_question)
+tf_idf_question_matrice = matrice_tf_idf_question(question_traitee, idf_resultats)
+print("Les scores tf-idf sont : ", tf_idf_question_matrice)
+print("\n")
+
+afficher_matrice_tf_idf_question(tf_idf_question_matrice)
 print("\n")
 
 
+
+
+# Calculs pour toute la question
+resultats_comparaison = produit_scalaire(tf_idf_question_matrice, tf_idf, fichiers)
+print("Produit scalaire pour la question :", resultats_comparaison)
+
+norme_question = calculer_norme_question(tf_idf_question_matrice)
+print("Norme du vecteur de la question :", norme_question)
+
+similarite = calculer_similarite_cosinus(tf_idf_question_matrice, tf_idf, fichiers)
+print("Similarité cosinus pour la question :", similarite)
+
+document_pertinent = trouver_document_le_plus_pertinent(tf_idf_question_matrice, tf_idf, fichiers)
+print("Document le plus pertinent pour la question :", document_pertinent)
+
+# Trouver le mot avec le score TF-IDF le plus élevé dans la question
+mot_important = mot_avec_score_tfidf_le_plus_eleve(tf_idf_question_matrice)
+print("Mot le plus important dans la question :", mot_important)
+
+# Trouver la phrase contenant le mot dans le document pertinent
+phrase_reponse = trouver_phrase_contenant_mot(source_directory, document_pertinent, mot_important)
+
+reponse_personnalisee = personnaliser_reponse(phrase_reponse)
+
+print("Réponse générée :", reponse_personnalisee)
 
 def affichage_menu():
         # Affichage des options du menu principal
@@ -475,7 +663,7 @@ def affichage_menu():
         elif choix == '6':
             # Afficher le premier président qui a parlé de ces mots cles
             mots_cles = ["climat", "écologie"]
-            afficher_premier_president(mots_cles)
+            premier_president_a_parler(mots_cles)
             affichage_menu()
         elif choix == '7':
             # Afficher les mots communs des présidents
